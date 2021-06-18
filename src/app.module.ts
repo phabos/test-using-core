87c553d7f6cbd6cd80d4056ab7bce7ae599cc695
FPrezat <fabien.prezat@orange.com>
2021-06-18 23:29:13 +0200

    fix err

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatModule } from './../node_modules/test-core-api/dist/cat/cat.module';
 
 @Module({
-  imports: [],
+  imports: [CatModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
